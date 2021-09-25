1d0d8b0e111333387f3da221d9f8af7d0b1b1637
majdmo9 <62933040+majdmo9@users.noreply.github.com>
2021-09-25 20:08:24 +0300

    last commit
    Signed-off-by: majdmo9 <62933040+majdmo9@users.noreply.github.com>

--- dist/tic-tac-toe/pages/_app.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../styles/globals.css");
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps}/>;
-}
-exports.default = MyApp;
